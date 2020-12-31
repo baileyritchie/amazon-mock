dcfa8bf5c2e983b239f1e89073bf5591e3e33b81
Bailey <baileylritchie@gmail.com>
2020-12-31 15:22:39 -0600

    added axios to fetch backend data

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Product from '../Components/Product'
-import data from '../data';
+import axios from 'axios';
+import LoadingBox from '../Components/LoadingBox';
+import MessageBox from '../Components/MessageBox';
+
 export default function HomeScreen() {
+  const [products,setProducts] = useState([]);
+  const [loading,setLoading] = useState(false);
+  const [error,setError] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const {data} = await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      } catch(err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  },[]);
   return (
-    <div className="row center">
-      {data.products.map((product) => (
-          <Product key={product._id} product={product}/> 
-      ))}
-    </div>
+     <div>
+      {loading ? <LoadingBox/> 
+        : error ? <MessageBox variant="danger">{error}</MessageBox>
+        : <div className="row center">
+        {products.map((product) => (
+            <Product key={product._id} product={product}/> 
+        ))}
+        </div>}
+     </div>
+    
   )
 }
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -43,7 +43,7 @@ export default function ProductScreen(props) {
                   <div>Status</div>
                   <div>{product.countInStock> 0 ? 
                     (<span className="success">In Stock</span>) 
-                    : (<span className="error">Unavailable</span>)}</div>
+                    : (<span className="danger">Unavailable</span>)}</div>
                 </div>
               </li>
               
