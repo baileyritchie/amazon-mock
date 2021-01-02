3e7914a3096c642336a22ffc1d9fc849b665aacd
Bailey <baileylritchie@gmail.com>
2021-01-02 16:05:38 -0600

    converting products data to fetch and store in mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,16 @@
 // entry point for backend application
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
 import userRouter from './routers/userRouter.js';
 import dotenv from 'dotenv';
+import productRouter from './routers/productRouter.js';
 
 dotenv.config();
 const DB_URL = process.env.DB_URL;
 const app = express();
 const PORT = process.env.port || 5000;
 
-app.get('/api/products/:id', (req,res) => {
-  const product = data.products.find((product) => product._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({message: 'Product Not Found'})
-  }
-});
-
-app.get('/api/products', (req,res) => {
-  res.send(data.products);
-});
+app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 
 app.get('/', (req,res) => {
